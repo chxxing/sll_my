5a6679cf7c4edb921e58290a7cb77a74fa92efb1
chxxing <choing26@naver.com>
2022-08-31 17:49:35 +0900

    220831

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const slider = new Swiper();
+
+    document.querySelector('.Header .lang').addEventListener('click', (e) => {
+        const LG = e.currentTarget;
+        LG.classList.toggle('on');
+    });
+
+    window.addEventListener('scroll', () => {
+        let SCR = window.scrollY;
+        console.log(SCR);
+        SCR > 0
+            ? document.querySelector('.Header').classList.add('on')
+            : document.querySelector('.Header').classList.remove('on')
+    });
+
+    const MainSlider = new Swiper('.swiper', {
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        loop: true,
+        effect: "fade",
+        speed: 1000,
+    });
 });
